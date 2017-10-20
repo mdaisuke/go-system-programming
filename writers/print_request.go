04abd8dd62c612d0f160aa1aa1776e1fc3217b17
mdaisuke <daisuke.mori@me.com>
2017-10-21 07:54:57 +0900

    io.Writer samples

---
+++ writers/print_request.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+	"os"
+)
+
+func main() {
+	request, err := http.NewRequest("GET", "http://ascii.jp", nil)
+	if err != nil {
+		panic(err)
+	}
+	request.Header.Set("X-TEST", "can add header")
+	request.Write(os.Stdout)
+}
